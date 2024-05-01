4f3d1ebe726d404a8b72d2ad01c076e8917d99fc
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-05-01 11:59:21 +0200

    create mnist classifier and dataset generator

---
+++ src/dataset/mnist.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import load_img, img_to_array, to_categorical
+
+
+def prepare_mnist_dataset(train_ratio=0.8):
+    (default_x_train, default_y_train), (default_x_test, default_y_test) = tf.keras.datasets.mnist.load_data()
+
+    # Concatenate the training and testing sets
+    x = np.concatenate([default_x_train, default_x_test])
+    y = np.concatenate([default_y_train, default_y_test])
+
+    # Split the combined dataset into training and testing sets
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=(1 - train_ratio))
+
+    # reduce images to vector of pixels. 28×28 image will be 784 pixel input values
+    x_train = x_train.reshape((x_train.shape[0], -1))
+    x_test = x_test.reshape((x_test.shape[0], -1))
+
+    # # reduce memory requirements by forcing precision of the pixel values to be 32 bit
+    # x_train = x_train.astype('float32')
+    # x_test = x_test.astype('float32')
+
+    print('Training images size: ', x_train.shape[0])
+    print('Training input size:', x_train.shape[1])
+    print('Training labels size:', y_train.shape[0])
+
+    print('Testing images size: ', x_test.shape[0])
+    print('Testing input size:', x_test.shape[1])
+    print('Testing labels size:', y_test.shape[0])
+
+    # normalize input (pixels are gray scale between 0 and 255)
+    x_train = x_train / 255
+    x_test = x_test / 255
+    print('Normalized result', x_train.shape, x_test.shape)
+
+    # One-hot encoding of output: transform the vector of class integers into a label binary variables
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
+    print('Output shape', y_train.shape)
+
+    return x_train, x_test, y_train, y_test
---
+++ src/image-classifier/image-classifier.py
@@ -0,0 +1,50 @@
+import sys
+import os
+
+module_to_import = os.path.join(os.path.dirname(sys.path[0]), 'dataset')
+print(module_to_import)
+sys.path.append(module_to_import)
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import SGD
+
+from mnist import prepare_mnist_dataset
+
+
+def neural_network(num_classes, num_pixels):
+    model = Sequential()
+
+    # input layer
+    model.add(Input(shape=(num_pixels,)))
+
+    # hidden layer
+    model.add(Dense(units=256, activation='relu'))
+
+    # hidden layer
+    model.add(Dense(units=64, activation='relu'))
+
+    # output layer
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.summary()
+    return model
+
+
+def train_model(model, x_train, y_train, learning_rate=0.001, epochs=5, batch_size=32):
+    # Optimizer: SGD (Stochastic Gradient Descent)
+    opt = SGD(learning_rate=learning_rate)
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1)
+
+
+def test_model(model, x_test, y_test):
+    scores = model.evaluate(x_test, y_test, verbose=1)
+    print("Error: ", (100 - scores[1] * 100), "%")
+
+
+x_train, x_test, y_train, y_test = prepare_mnist_dataset()
+model = neural_network(10, 784)
+train_model(model, x_train, y_train)
+test_model(model, x_test, y_test)
