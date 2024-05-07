c5152223fbbf1766286eda711003e69f6b7065ea
Lyadalachanchu <56978360+Lyadalachanchu@users.noreply.github.com>
2024-05-07 12:03:38 +0200

    Update src/utils.py
    Co-authored-by: Henry <69983179+1henrypage@users.noreply.github.com>

--- src/utils.py
+++ src/utils.py
@@ -98,9 +98,9 @@ def fed_avg(local_weights, dataset_size_per_client):
     for i, dictionary in enumerate(local_weights):
         for key, tensor in dictionary.items():
             if key not in avg_dict:
-                avg_dict[key] = tensor.clone() * dataset_size_per_client[i]/ sum_dataset
+                avg_dict[key] = tensor.clone() * (dataset_size_per_client[i]/ sum_dataset)
             else:
-                avg_dict[key] += tensor.clone() * dataset_size_per_client[i]/sum_dataset
+                avg_dict[key] += tensor.clone() * (dataset_size_per_client[i]/sum_dataset)
     return avg_dict
 
 
