b742e77e055660ff02f9aa7b6f3a8105047d7c16
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-05-13 21:08:18 +0200

    attempt cifar image generation

--- src/vae/cifar_vae.py
+++ src/vae/cifar_vae.py
@@ -1,11 +1,8 @@
-from src.utils import kl_loss, vae_loss_fn, vae_classifier_loss_fn
-
 import torch
 from torch import Tensor
 import torch.nn.functional as F
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from torch.distributions.normal import Normal
 
 
 class Reshape(nn.Module):
@@ -32,7 +29,6 @@ class VaeAutoencoderClassifier(nn.Module):
         self.dim_encoding = dim_encoding
         self.latent_space_vector = None
         self.encodings = None
-        self.z_dist = None
         self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
         self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
         self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
@@ -56,8 +52,10 @@ class VaeAutoencoderClassifier(nn.Module):
         x = F.silu(self.conv2(x))
         x = self.conv3(x)
         x = self.flatten(x)
-        self.encodings = x
-        return self.reparameterize(x)
+        mu = self.mu(x)
+        sigma = self.sigma(x)
+        z = self.reparameterize(mu, sigma)
+        return mu, sigma, z
 
     def decode(self, z):
         z = self.linear(z)
@@ -69,24 +67,15 @@ class VaeAutoencoderClassifier(nn.Module):
         z = F.sigmoid(z)
         return z
 
-    def reparameterize(self, encodings: Tensor) -> Tensor:
-        mu = self.mu(encodings)
-        sigma = F.softplus(self.sigma(encodings))
-
-        # both should be of (<batch_size>, <dim_encoding>)
-        assert mu.shape == sigma.shape
-
-        z_dist = Normal(mu, sigma)
-        self.z_dist = z_dist
-        z = z_dist.rsample()
-        self.latent_space_vector = z
+    def reparameterize(self, mean, var):
+        eps = torch.randn(mean.size(0), mean.size(1))
+        z = mean + eps * torch.exp(var / 2.)
         return z
 
     def forward(self, x):
-        z = self.encode(x)
+        mu, sigma, z = self.encode(x)
         decoded = self.decode(z)
-        # print(decoded.shape)
-        return decoded
+        return mu, sigma, decoded
 
     def train_model(
             self,
@@ -96,9 +85,7 @@ class VaeAutoencoderClassifier(nn.Module):
             epochs=5,
             learning_rate=0.01
     ) -> tuple[nn.Module, list]:
-        vl_fn = vae_loss_fn(beta)
-        kl_div_fn = kl_loss()
-
+        mse = nn.MSELoss(reduction='sum')
         vae_classifier_model = self.to('cuda')
         optimizer = torch.optim.Adam(
             params=vae_classifier_model.parameters(),
@@ -113,24 +100,26 @@ class VaeAutoencoderClassifier(nn.Module):
         for epoch in range(epochs):
             i = 0
             for input, _ in training_dataloader:
-                input = input.to('cuda')
-                output = vae_classifier_model(input)
+                # input = input.to('cuda')
+                mean, var, output = vae_classifier_model(input)
+
+                optimizer.zero_grad()
 
                 # loss function to back-propagate on
-                loss = vl_fn(input, output, self.z_dist)
+                kl_loss = torch.mean(-0.5 * torch.sum(1 + var - mean**2 - torch.exp(var),axis=1),axis=0)
+                vae_loss = mse(output, input) + kl_loss
 
                 # back propagation
-                optimizer.zero_grad()
-                loss.backward()
+                vae_loss.backward()
                 optimizer.step()
                 i += 1
                 if i % 100 == 0:
                     # append vae loss
-                    vae_loss_li.append(loss.item())
+                    vae_loss_li.append(vae_loss.item())
 
                     # calculate KL divergence loss
                     kl_loss_li.append(
-                        kl_div_fn(self.z_dist)
+                        kl_loss
                     )
             print("Finished epoch: ", epoch + 1)
         return (
@@ -148,4 +137,4 @@ class VaeAutoencoderClassifier(nn.Module):
         assert(input_sample.shape[0] == n_samples)
 
         output = self.decode(input_sample)
-        return output.reshape(-1, 3, 32, 32)
+        return output.reshape(-1, 32, 32, 3)
